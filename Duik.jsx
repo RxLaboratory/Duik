a7ec4fe4ebf6ee3a8866c52d92d64d7aade3bbcf
Nicolas Dufresne <git@duduf.com>
2015-11-07 16:49:43 +0100

    fixed footroll orientation #189

--- Duik.jsx
+++ Duik.jsx
@@ -30,7 +30,7 @@ along with Duik. If not, see <http://www.gnu.org/licenses/>.
 (function (thisObj)
 {
 //=========================
-var version = '15.04';
+var version = '15.05';
 //=========================
 
 //=================================
