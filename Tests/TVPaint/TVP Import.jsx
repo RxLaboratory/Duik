b97f9cee29da15b926519d26b2c19da017011279
Duduf <git@duduf.com>
2017-05-19 15:47:44 +0200

    added licence

--- Tests/TVPaint/TVP Import.jsx
+++ Tests/TVPaint/TVP Import.jsx
@@ -1,3 +1,26 @@
+/*
+	TVP Import
+	Copyright (c) 2017 Rainbox Productions
+	https://rainboxprod.coop
+
+	Based on MJ_TVPAINT_IMPORT_1.0.3.jsx by Mads Juul
+	https://forum.tvpaint.com/viewtopic.php?f=11&t=6978
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include libduik.jsxinc
 
 /**
