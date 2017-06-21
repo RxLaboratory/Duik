fff14276be3a2f00ac6f2832287e3d2520fa1ee3
Duduf <git@duduf.com>
2017-06-21 20:21:37 +0200

    changed version to 15.52
    Former-commit-id: 6b56a6ba3ddb3f31d41a0cf0f06d5c4c8a3025a3

--- src/Duik.jsx
+++ src/Duik.jsx
@@ -30,7 +30,7 @@ along with Duik. If not, see <http://www.gnu.org/licenses/>.
 (function (thisObj)
 {
 //=========================
-var version = '15.51';
+var version = '15.52';
 //=========================
 
 //=================================
