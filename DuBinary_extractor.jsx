ca6118dac46062c3bcf08c0b6048c641f002ba87
Duduf <git@duduf.com>
2017-11-01 12:17:47 +0100

    initial commit

---
+++ DuBinary_extractor.jsx
@@ -0,0 +1,21 @@
+﻿/*
+	DuBinary Extractor
+	Small tool to extract converted binary files
+	Copyright (c) 2017 Nicolas Dufresne, Rainbox Productions
+	https://rainboxprod.coop
+*/
+
+(function ()
+{
+	#include "libs/DuBinaryLib.jsxinc"
+
+	var file = File.openDialog("Open include file.","Javascript files: *.jsx;*.jsxinc");
+	if (!file) return;
+
+	var obj = $.evalFile(file);
+
+	var outputFileName = file.path + '/' + obj.category + '/' + obj.fileName;
+
+	DuBinary.toFile(obj,outputFileName);
+
+})();
