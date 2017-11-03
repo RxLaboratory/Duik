f41a3f92d2beb1b1a7930de3c7f63d57253a0259
Nicolas Dufresne <git@duduf.com>
2017-11-03 15:50:31 +0100

    use rainbox UI

--- DuBuilder/src/main.cpp
+++ DuBuilder/src/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "frameless.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    FrameLess f(&w);
     w.show();
 
     return a.exec();
