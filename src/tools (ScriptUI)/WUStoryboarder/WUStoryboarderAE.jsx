b24514742b7bd23b30425f041704f218bb435193
Duduf <git@duduf.com>
2018-02-18 18:31:19 +0100

    WUStoryboarderAE 0.1 Release

--- src/tools (ScriptUI)/WUStoryboarder/WUStoryboarderAE.jsx
+++ src/tools (ScriptUI)/WUStoryboarder/WUStoryboarderAE.jsx
@@ -1,13 +1,29 @@
+/**
+ * @overview Wonderunit Storyboarder project importer for Adobe After EFfects.
+ * @author Nicolas Dufresne and contributors
+ * @copyright 2018 Nicolas Dufresne
+ * @version 0.1
+ * @name WUStoryboarderAE
+ * @license GPL-3.0 <br />
+ * WUStoryboarderAE is free software: you can redistribute it and/or modify<br />
+ * it under the terms of the GNU General Public License as published by<br />
+ * the Free Software Foundation, either version 3 of the License, or<br />
+ * (at your option) any later version.<br />
+ *<br />
+ * WUStoryboarderAE is distributed in the hope that it will be useful,<br />
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of<br />
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br />
+ * GNU General Public License for more details.<br />
+ *<br />
+ * You should have received a copy of the GNU General Public License<br />
+ * along with WUStoryboarderAE. If not, see {@link http://www.gnu.org/licenses/}.
+ */
+
 (function(thisObj)
 {
-	#include "libs/JSON.jsxinc"
-	#include "libs/DuAEFLib.jsxinc"
-	#include "libs/DuAECoreLib.jsxinc"
-	#include "libs/DuJSLib.jsxinc"
-	#include "libs/DuScriptUILib.jsxinc"
-	#include "libs/DuWUStoryboarderLib.jsxinc"
+	#include "DuAEF.jsxinc"
 
-	var version = "0.1-Beta-1";
+	var version = "0.1";
 
 	//====== EVENTS ===============
 
