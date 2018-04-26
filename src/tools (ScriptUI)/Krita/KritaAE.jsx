5a313cc05b397169bd75f2dbea31090979a1c760
Duduf <git@duduf.com>
2018-04-26 21:14:59 +0200

    new Krita interchange tools

---
+++ src/tools (ScriptUI)/Krita/KritaAE.jsx
@@ -0,0 +1,9 @@
+﻿(function(thisObj) {
+
+#include 'E:/DEV SRC/DuAEF/src/DuAEF.jsxinc'
+
+var file = new File('H:/RAINBOX/DEV/AE/Duik/WIP/krita/boom.csv');
+
+DuAEF.Krita.importCSV(file);
+
+})(this);
