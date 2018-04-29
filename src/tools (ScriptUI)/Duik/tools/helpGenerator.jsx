fbfc8ea8464b16a0164101dcfbdc59669cce6312
Duduf <git@duduf.com>
2018-04-29 13:28:34 +0200

    added tools/helpGenerator to generate help strings from doc files
    Former-commit-id: f3fc45aac357da645806f561ab3afa36198d3fc7

---
+++ src/tools (ScriptUI)/Duik/tools/helpGenerator.jsx
@@ -0,0 +1,70 @@
+/**
+This small script is used to generate the help strings used in the contextual help of Duik based on the files found in the 'help' folder.
+It will overwrite the file Duik16_helpStrings.jsxinc.
+Each file will be converted to a string, stored in a variable named after the filename + "Help" which can then be used in the source of Duik.
+*/
+
+(function()
+{
+	function getFileString(file)
+	{
+		var data = '';
+		if (!file.open('r')) return data;
+		data = file.read();
+		file.close();
+		//stringify
+		//remove (starting and trailing whitespaces, and carriage returns
+		data = data.replace(/^\s+|\r+|\s+$/g,'');
+		//quotes
+		data = data.replace(/"/g,'\\"');
+		//line feeds
+		data = data.replace(/\n/g,'\\n');
+
+		data = '"' + data + '"';
+
+		return data;
+	}
+
+	function isFolder(file)
+	{
+		return file instanceof Folder;
+	}
+
+	function isFile(file)
+	{
+		return file instanceof File;
+	}
+
+	var scriptFolder = new File($.fileName).parent.parent;
+	var helpFolder = new Folder(scriptFolder.absoluteURI + '/help');
+	var helpContent = helpFolder.getFiles(isFolder);
+
+	var data = '';
+
+	for (var i = 0, num = helpContent.length; i < num; i++)
+	{
+		var folder = helpContent[i];
+		data += '// ==== ' + folder.name + ' ====\n\n';
+		var files = folder.getFiles(isFile);
+		for (var j = 0, numJ = files.length; j < numJ; j++)
+		{
+			var file = files[j];
+			data += 'var ' + file.name + 'Help = ';
+			data += getFileString(file);
+			data += ';\n\n';
+		}
+	}
+
+	//write data
+
+	var helpFile = new File(scriptFolder.absoluteURI + '/Duik16_helpStrings.jsxinc');
+	if (!helpFile.open('w'))
+	{
+		alert("Couldn't open helpStrings file.");
+		return;
+	}
+	helpFile.write(data);
+	helpFile.close();
+
+	alert("helpStrings file correctly generated");
+})();
