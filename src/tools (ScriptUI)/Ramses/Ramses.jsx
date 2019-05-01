f90dc49870424e8c5075d32f8647fbb90b0510e8
Duduf <git@duduf.com>
2019-05-01 17:52:40 +0800

    improved UI with images

--- src/tools (ScriptUI)/Ramses/Ramses.jsx
+++ src/tools (ScriptUI)/Ramses/Ramses.jsx
@@ -20,13 +20,19 @@
 
 //encapsulate the script in a function to avoid global variables
 ( function( thisObj ) {
+
     //================
     var version = '0.0.0-a';
     var scriptName = "Ramses";
     //================
+
+    //=============== INCLUDES ================
+
     #include DuAEF.jsxinc
 
-    // ================ SETTINGs ==============
+    #include 'Ramses_required/Ramses_w25icons.jsxinc'
+
+    // ================ SETTINGS ==============
 
     // Restores the default settings
     //TODO move in DuAEF.DuSettings along with a function to set defaults
@@ -38,7 +44,7 @@
     }
 
     // Create/load settings
-    var settings = new DuSettings( "Ramses" );
+    var settings = new DuSettings( scriptName );
     setDefaultSettings( settings );
 
 
@@ -49,15 +55,13 @@
     // ================ FUNCTIONS =============
     //MAIN
 
-//TODO mark versions as WIP or Published
-    function saveProject(increment, publish) {
+    //TODO mark versions as WIP or Published
+    function saveProject( increment, publish ) {
 
-        if (typeof increment === 'undefined') increment = false;
-        if (typeof publish === 'undefined') publish = false;
+        if ( typeof increment === 'undefined' ) increment = false;
+        if ( typeof publish === 'undefined' ) publish = false;
 
         var projectFile = app.project.file;
-        var projectPath = projectFile.path;
-        var projectName = '';
 
         // == save WIP project ==
 
@@ -68,12 +72,16 @@
                 projectName = projectName.substring( 0, projectName.length - settings.data.wipName.length - 1 );
             }
         } else {
-            //todo demander destination
+            //todo demander destination et save
+            alert('Please save this project with After Effects once before using Ramses.')
             return;
         }
 
+        var projectPath = projectFile.path;
+        var projectName = '';
+
         var saveName = projectPath + '/' + projectName;
-        if (!publish) saveName += '_' + settings.data.wipName;
+        if ( !publish ) saveName += '_' + settings.data.wipName;
         saveName += '.aep';
         projectFile = new File( saveName );
         app.project.save( projectFile );
@@ -81,25 +89,23 @@
         // == Copy version ==
 
         var versionFolder = new Folder( projectPath + '/' + settings.data.versionFolderName );
-        
+
         // gets current version
         var currentVersion = 0;
 
         if ( versionFolder.exists ) {
             // gets all exsting versions
-            var projectVersionFiles = versionFolder.getFiles(projectName + "_v*" + ".aep");
+            var projectVersionFiles = versionFolder.getFiles( projectName + "_v*" + ".aep" );
             for ( var i = 0, num = projectVersionFiles.length; i < num; i++ ) {
-                var f = projectVersionFiles[i];
-                if (DuAEF.DuJS.Fs.isFile(f))
-                {
+                var f = projectVersionFiles[ i ];
+                if ( DuAEF.DuJS.Fs.isFile( f ) ) {
                     var fName = DuAEF.DuJS.Fs.getBasename( f );
-                    var v = fName.replace(projectName + "_v", '');
-                    v = v.replace(".aepx",'');
-                    v = v.replace(".aep",'');
-                    v = parseInt(v, 10);
-                    if (!isNaN(v))
-                    {
-                        if (v > currentVersion) currentVersion = v;
+                    var v = fName.replace( projectName + "_v", '' );
+                    v = v.replace( ".aepx", '' );
+                    v = v.replace( ".aep", '' );
+                    v = parseInt( v, 10 );
+                    if ( !isNaN( v ) ) {
+                        if ( v > currentVersion ) currentVersion = v;
                     }
                 }
             }
@@ -111,49 +117,53 @@
         if ( increment || currentVersion == 0 ) currentVersion++;
 
         // copy version
-        var currentVersionString = DuAEF.DuJS.Number.convertToString ( currentVersion , 3 );
-        var successful = projectFile.copy(versionFolder.absoluteURI + "/" + projectName + "_v" + currentVersionString + ".aep");
-        if (!successful) alert("Warning - Error writing file\nThe version could not be backed up properly.");
+        var currentVersionString = DuAEF.DuJS.Number.convertToString( currentVersion, 3 );
+        var successful = projectFile.copy( versionFolder.absoluteURI + "/" + projectName + "_v" + currentVersionString + ".aep" );
+        if ( !successful ) alert( "Warning - Error writing file\nThe version could not be backed up properly." );
         return successful;
     }
 
-//TODO clean : removes all wip versions but the highest, and main wip if a more recent final is found
+    //TODO clean : removes all wip versions but the highest, and main wip if a more recent final is found
 
     //UI EVENTS
 
-    function ui_saveButton_clicked ()
-    {
-        saveProject(false);
+    function ui_saveButton_clicked() {
+        saveProject( false );
     }
 
-    function ui_saveIncrementalButton_clicked ()
-    {
-        saveProject(true);
+    function ui_saveIncrementalButton_clicked() {
+        saveProject( true );
     }
 
-    function ui_publishButton_clicked()
-    {
-        saveProject(true, true);
+    function ui_publishButton_clicked() {
+        saveProject( true, true );
     }
 
 
     // _______ UI SETUP _______
     var ui = DuAEF.DuScriptUI.createUI( thisObj, scriptName );
 
+    var ui_buttonsGroup = DuAEF.DuScriptUI.addGroup( ui.contents );
+    var ui_bottomLine = DuAEF.DuScriptUI.addGroup( ui.contents );
+
     // ============ UI CONTENT =================
-    ui.contents.orientation = 'row';
+
     // Save
-    ui_saveButton = DuAEF.DuScriptUI.addButton( ui.contents, 'S', '', 'Save project' );
+    ui_saveButton = DuAEF.DuScriptUI.addButton( ui_buttonsGroup, '', DuAEF.DuBinary.toFile( w25_save2_l ), 'Save project', DuAEF.DuBinary.toFile( w25_save2_r ) );
     ui_saveButton.alignment = [ 'left', 'top' ];
     // Save New Version
-    ui_saveIncrementalButton = DuAEF.DuScriptUI.addButton( ui.contents, 'S+', '', 'Save project and increment version' );
+    ui_saveIncrementalButton = DuAEF.DuScriptUI.addButton( ui_buttonsGroup, '', DuAEF.DuBinary.toFile( w25_saveinc_l ), 'Save project and increment version', DuAEF.DuBinary.toFile( w25_saveinc_r ) );
     ui_saveIncrementalButton.alignment = [ 'left', 'top' ];
     // Publish
-    ui_publishButton = DuAEF.DuScriptUI.addButton( ui.contents, 'P', '', 'Validate and publish project' );
+    ui_publishButton = DuAEF.DuScriptUI.addButton( ui_buttonsGroup, '', DuAEF.DuBinary.toFile( w25_publish_l ), 'Validate and publish project', DuAEF.DuBinary.toFile( w25_publish_r ) );
     ui_publishButton.alignment = [ 'left', 'top' ];
     // Clean
-    ui_cleanButton = DuAEF.DuScriptUI.addButton( ui.contents, 'C', '', 'Cleans the version folder' );
+    ui_cleanButton = DuAEF.DuScriptUI.addButton( ui_buttonsGroup, '', DuAEF.DuBinary.toFile( w25_broom_l ), 'Cleans the version folder', DuAEF.DuBinary.toFile( w25_broom_r ) );
     ui_cleanButton.alignment = [ 'left', 'top' ];
+    ui_cleanButton.enabled = false;
+    // Version
+    ui_versionLabel = DuAEF.DuScriptUI.addStaticText( ui_bottomLine, 'v' + version + ' | rainboxlab.org' );
+    ui_versionLabel.alignment = [ 'left', 'bottom' ];
 
     // ========== CONNECT EVENTS ===============
 
