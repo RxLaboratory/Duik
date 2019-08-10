9b0df865ae5c7d949c29a3fa3ea25bf12d1a6d33
Duduf <git@duduf.com>
2019-08-10 11:55:25 +0200

    fixed #3
    Former-commit-id: f2aa2d9f9b6c6613ce95d4832ffd5012ad87df4d

--- src/tools_(ScriptUI)/Duik/Duik Bassel.1.jsx
+++ src/tools_(ScriptUI)/Duik/Duik Bassel.1.jsx
@@ -1,8 +1,8 @@
-#include "duik_required/Duik16_header.jsxinc"
+#include "duik_required/Duik16_header.jsxinc";
 
 var thisScriptFile = new File($.fileName);
 var standAlone = false;
 
-#include "duik_required/Duik16_mainUI.jsxinc"
+#include "duik_required/Duik16_mainUI.jsxinc";
 
-#include "duik_required/Duik16_footer.jsxinc"
+#include "duik_required/Duik16_footer.jsxinc";
