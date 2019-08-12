02efdf566343f2907e6297e1ef420b173f9fdace
Duduf <git@duduf.com>
2019-08-12 10:20:12 +0200

    fix #495

--- src/tools_(ScriptUI)/Duik/Duik Animation.jsx
+++ src/tools_(ScriptUI)/Duik/Duik Animation.jsx
@@ -1,6 +1,5 @@
 #include "duik_required/Duik16_header.jsxinc"
 
-var thisScriptFile = new File($.fileName);
 var standAlone = true;
 #include "duik_required/Duik16_animation.jsxinc"
 ui_palette.alignChildren = ['fill','fill'];
