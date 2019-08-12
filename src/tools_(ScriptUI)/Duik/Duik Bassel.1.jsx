daba2d30a43e3bbaabc4c9b50c38766c51319de7
Duduf <git@duduf.com>
2019-08-12 10:20:12 +0200

    fix #495
    Former-commit-id: 02efdf566343f2907e6297e1ef420b173f9fdace

--- src/tools_(ScriptUI)/Duik/Duik Bassel.1.jsx
+++ src/tools_(ScriptUI)/Duik/Duik Bassel.1.jsx
@@ -1,6 +1,5 @@
 #include "duik_required/Duik16_header.jsxinc";
 
-var thisScriptFile = new File($.fileName);
 var standAlone = false;
 
 #include "duik_required/Duik16_mainUI.jsxinc";
--- src/tools_(ScriptUI)/Duik/Duik Notes.jsx
+++ src/tools_(ScriptUI)/Duik/Duik Notes.jsx
@@ -1,6 +1,5 @@
 #include "duik_required/Duik16_header.jsxinc"
 
-var thisScriptFile = new File($.fileName);
 var notes_ui_palette = ui_palette;
 var standAlone = true;
 #include "duik_required/Duik16_notes.jsxinc"
