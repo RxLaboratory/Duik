595e1096d6f8ac45d61fd890349c4e1901537f69
Duduf <git@duduf.com>
2019-08-20 09:42:51 +0200

    fix missing (

--- src/tools_(ScriptUI)/Duik/tools/Uninstall Duik.jsx
+++ src/tools_(ScriptUI)/Duik/tools/Uninstall Duik.jsx
@@ -2,9 +2,13 @@
 //Removes all stuff left behind by Duik
 //You'll have to remove the panels from ScriptUI Panels by yourself.
 
-function ()
+(function ()
 {
     #include "../DuAEF.jsxinc"
+    
+    
     #include "../duik_required/Duik16_reinit.jsxinc"
+    
+    
     wipeDuik();
 })();
