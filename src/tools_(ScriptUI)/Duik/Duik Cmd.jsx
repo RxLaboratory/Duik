4efa7dc78332dfd4f759f9f0db7315a8a4c9f2c2
Duduf <git@duduf.com>
2019-08-22 19:39:01 +0200

    Command Line Interface proof of concept
    Former-commit-id: 5299cc1163607ec9168fa49f2c59f8703f8af817

---
+++ src/tools_(ScriptUI)/Duik/Duik Cmd.jsx
@@ -0,0 +1,8 @@
+#include "duik_required/Duik16_header.jsxinc";
+
+var standAlone = true;
+var noSettings = true;
+
+#include "duik_required/Duik16_cmd.jsxinc";
+
+#include "duik_required/Duik16_footer.jsxinc";
