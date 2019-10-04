5114cf16a0a201e835d2f6c2b97ec922ea933191
Duduf <git@duduf.com>
2019-10-04 12:01:46 +0200

    new kleaner panel UI
    Former-commit-id: a7e029325e01fc979a58188a6114ee3c5a55923c

---
+++ src/Duik Kleaner.jsx
@@ -0,0 +1,9 @@
+#include "duik_required/Duik16_header.jsxinc"
+
+var standAlone = true;
+#include "duik_required/Duik16_kleaner.jsxinc"
+ui_palette.alignChildren = ['fill','fill'];
+ui_buildKleaner( ui_palette );
+
+#include "duik_required/Duik16_footer.jsxinc"
+
