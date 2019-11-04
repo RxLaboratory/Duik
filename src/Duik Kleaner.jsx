e9efc0ce175d0c9829a6adc80345c179a8a1b50f
Duduf <git@duduf.com>
2019-11-04 12:34:43 +0100

    removed kleaner standalone panel
    Former-commit-id: a53371e739e7164382041869e201b275885f06f7

--- src/Duik Kleaner.jsx
+++
@@ -1,9 +0,0 @@
-#include "duik_required/Duik16_header.jsxinc"
-
-var standAlone = true;
-#include "duik_required/Duik16_kleaner.jsxinc"
-ui_palette.alignChildren = ['fill','fill'];
-ui_buildKleaner( ui_palette );
-
-#include "duik_required/Duik16_footer.jsxinc"
-
