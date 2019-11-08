dfc72b06732f1b325e9d63381e7bdb1af6e56337
Duduf <git@duduf.com>
2019-11-08 19:26:02 +0100

    moved IO outside of Duik, and reoganised tools panel

--- src/Duik Import-Export.jsx
+++
@@ -1,9 +0,0 @@
-#include "duik_required/Duik16_header.jsxinc"
-
-var standAlone = true;
-#include "duik_required/Duik16_io.jsxinc"
-ui_palette.alignChildren = ['fill','fill'];
-ui_buildIO( ui_palette );
-
-#include "duik_required/Duik16_footer.jsxinc"
-
