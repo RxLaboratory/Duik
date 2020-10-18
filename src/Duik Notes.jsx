8b15d642c60fe7051aa94dbbc392715866965d24
Duduf <git@duduf.com>
2020-10-18 21:00:35 +0200

    implemented new mainPalette from DuAEF and sanity checks

--- src/Duik Notes.jsx
+++ src/Duik Notes.jsx
@@ -1,6 +1,6 @@
 #include "duik_required/Duik16_header.jsxinc"
 
-var notes_ui_palette = ui_palette;
+var notes_ui_palette = ui_palette.mainGroup;
 var standAlone = true;
 #include "duik_required/Duik16_notes.jsxinc"
 
