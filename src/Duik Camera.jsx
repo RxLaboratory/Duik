01b5c5e10f393427c2fed923d8696b5c9e5d7242
Duduf <git@duduf.com>
2020-10-18 21:00:35 +0200

    implemented new mainPalette from DuAEF and sanity checks
    Former-commit-id: 8b15d642c60fe7051aa94dbbc392715866965d24

--- src/Duik Camera.jsx
+++ src/Duik Camera.jsx
@@ -2,8 +2,7 @@
 
 var standAlone = true;
 #include "duik_required/Duik16_camera.jsxinc"
-ui_palette.alignChildren = ['fill','fill'];
-ui_buildCamera( ui_palette );
+ui_buildCamera( ui_palette.mainGroup );
 
 #include "duik_required/Duik16_footer.jsxinc"
 
