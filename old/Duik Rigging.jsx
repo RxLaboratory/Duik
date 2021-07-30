3db9e961a6212c8b0ef94bb28bdb44a9c5805411
Duduf <git@duduf.com>
2021-07-30 16:37:57 +0200

    prepared 17.0.0 panels

---
+++ old/Duik Rigging.jsx
@@ -0,0 +1,8 @@
+#include "duik_required/Duik16_header.jsxinc"
+
+var standAlone = true;
+#include "duik_required/Duik16_rigging.jsxinc"
+ui_buildRigging( ui_palette.mainGroup );
+
+#include "duik_required/Duik16_footer.jsxinc"
+
