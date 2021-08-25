f9419b678f6c4efe80c50b94debe694cbce19113
Duduf <git@duduf.com>
2021-08-25 11:48:36 +0200

    17.0.0-Alpha1
    Former-commit-id: 813ab996208a276180e7532b9819931033e66480

--- docs/scripts/linenumber.js
+++ docs/scripts/linenumber.js
@@ -1,30 +1,29 @@
-/*global document */
 (function() {
-    var source = document.getElementsByClassName('prettyprint source linenums');
-    var i = 0;
-    var lineNumber = 0;
-    var lineId;
-    var lines;
-    var totalLines;
-    var anchorHash;
-    var lineNumberHTML = '';
+    var counter = 0;
+    var numbered;
+    var source = document.getElementsByClassName('prettyprint source');
 
     if (source && source[0]) {
-        anchorHash = document.location.hash.substring(1);
-        lines = source[0].getElementsByTagName('li');
-        totalLines = lines.length;
+        var linenums = config.linenums;
 
-        for (; i < totalLines; i++) {
-            lineNumber++;
-            lineId = 'line' + lineNumber;
-            lines[i].id = lineId;
+        if (linenums) {
+            source = source[0].getElementsByTagName('ol')[0];
 
-            lineNumberHTML = '<span class="number">' + (i + 1) + ' : </span>';
+            numbered = Array.prototype.slice.apply(source.children);
+            numbered = numbered.map(function(item) {
+                counter++;
+                item.id = 'line' + counter;
+            });
+        } else {
+            source = source[0].getElementsByTagName('code')[0];
 
-            lines[i].insertAdjacentHTML('afterBegin', lineNumberHTML);
-            if (lineId === anchorHash) {
-                lines[i].className += ' selected';
-            }
+            numbered = source.innerHTML.split('\n');
+            numbered = numbered.map(function(item) {
+                counter++;
+                return '<span id="line' + counter + '"></span>' + item;
+            });
+
+            source.innerHTML = numbered.join('\n');
         }
     }
 })();
