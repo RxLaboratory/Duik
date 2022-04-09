6c8ecd0917bb6f0e78dbde343020e2392fa6d46d
Duduf <git@duduf.com>
2022-04-09 16:40:32 +0200

    finished libraries refactor
    Former-commit-id: 0aebcd59ad834791f7f6ce84e4f7416675f1918f

--- Duik Script Library.jsx
+++ Duik Script Library.jsx
@@ -4,8 +4,30 @@ var mainScriptFile = new File($.fileName);
 
 #include "inc/core.jsxinc"
 
+var mainStack = DuScriptUI.group(ui.mainGroup, 'stacked' );
+mainStack.alignment = ['fill','fill'];
+var libGroup = DuScriptUI.group(mainStack, 'column');
+libGroup.alignment = ['fill','fill'];
+var editorGroup = DuScriptUI.group(mainStack, 'column');
+editorGroup.alignment = ['fill','fill'];
+editorGroup.visible = false;
+
 #include "inc/scriptLibPanel.jsxinc"
-buildScriptLibPanel( ui.mainGroup );
+buildScriptLibPanel( libGroup, editorGroup );
+
+#include "inc/scriptEditorPanel.jsxinc"
+createSubPanel(
+    editorGroup,
+    DuScriptUI.String.SCRIPT_LIB,
+    libGroup,
+    false
+);
+buildScriptEditorUI( editorGroup );
+editorGroup.edit = function(content) {
+    editorGroup.editText.text = content;
+    libGroup.visible = false;
+    editorGroup.visible = true;
+};
 
 #include "inc/ui_show.jsxinc"
 
