ebeaa51a7ffcd0d062496a5f18a93070d148885f
Duduf <git@duduf.com>
2022-11-16 17:21:12 +0100

    fixed creating bezier ik when layers are not aligned

--- tests.jsx
+++ tests.jsx
@@ -3,5 +3,9 @@
     #include "inc/api_all.jsxinc"
     DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
 
-    Duik.Bone.bake(false, false, false);
+    var comp = app.project.activeItem;
+    var l = comp.layer(6);
+    var a = comp.layer(5);
+    var b = comp.layer(7);
+    alert( DuAELayer.angleFromLayers(l, a, b) );
 })();
