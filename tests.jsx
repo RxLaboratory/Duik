301de34cbc2dcb8ffd14d5753812f787ee5b09c3
Duduf <git@duduf.com>
2022-12-18 10:46:53 +0100

    smarter bone autoparent by name

--- tests.jsx
+++ tests.jsx
@@ -3,9 +3,7 @@
     #include "inc/api_all.jsxinc"
     DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
 
-    var comp = app.project.activeItem;
-    var l = comp.layer(6);
-    var a = comp.layer(5);
-    var b = comp.layer(7);
-    alert( DuAELayer.angleFromLayers(l, a, b) );
+    var limb = "shins";
+    var result = OCOBone.getNameSynonyms( limb, true );
+    alert( result.score );
 })();
