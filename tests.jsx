7f2ea29490cf549d94f6bcd69f7d27eb6b2c1af9
Duduf <git@duduf.com>
2022-12-22 10:35:03 +0100

    improved ctrl tagging & getCreate

--- tests.jsx
+++ tests.jsx
@@ -3,7 +3,16 @@
     #include "inc/api_all.jsxinc"
     DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
 
-    var limb = "shins";
-    var result = OCOBone.getNameSynonyms( limb, true );
-    alert( result.score );
+    var comp = app.project.activeItem;
+    var b1 = comp.layer(24);
+    var ctrl1 = comp.layer(8);
+    var b2 = comp.layer(10);
+    var ctrl2 = comp.layer(7);
+
+    var ctrls = [ctrl1, ctrl2];
+
+    var t = Duik.Controller.getCreate( b1, Duik.Layer.Type.FOOT, ctrls );
+    alert(t.name);
+    t = Duik.Controller.getCreate( b2, Duik.Layer.Type.FOOT, ctrls );
+    alert(t.name);
 })();
