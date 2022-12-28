7c90b2d8605f2993dde7edc30e158bbb50063388
Duduf <git@duduf.com>
2022-12-28 16:20:55 +0100

    Option to use Ctrls as Nulls or Draft mode

--- tests.jsx
+++ tests.jsx
@@ -1,10 +1,8 @@
 ﻿(function() {
 
-    //#include "inc/api_all.jsxinc"
-    //DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
+    #include "inc/api_all.jsxinc"
+    DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
 
-    #include "X:\\test.jsxinc";
-    alert("test");
-    //alert(DuString.chunk("avery\u00F0longstring", 6));
-    //preset_ctrl_foot.toJsxincFile("x:\\preset_ctrl_foot.jsxinc", undefined, false);
+    Duik.Controller.useNulls;
+    Duik.Controller.create(undefined, Duik.Controller.Type.NULL);
 })();
