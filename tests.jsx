73885282c7671c0ae640e61d6cb466e28b70677c
Duduf <git@duduf.com>
2022-12-31 17:03:54 +0100

    improved layer naming

--- tests.jsx
+++ tests.jsx
@@ -3,6 +3,20 @@
     #include "inc/api_all.jsxinc"
     DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
 
-    Duik.Controller.useNulls;
-    Duik.Controller.create(undefined, Duik.Controller.Type.NULL);
+    var comp = DuAEProject.getActiveComp();
+    if (!comp) return;
+
+    DuAE.beginUndoGroup("Sanitize names");
+
+    /*
+    for (var i = 1, ni = comp.numLayers; i <= ni; i++) {
+        Duik.Layer.sanitizeName( comp.layer(i) );
+    }
+    */
+
+    for (var i = 0, ni = comp.selectedLayers.length; i < ni; i++) {
+        Duik.Layer.setCharacterName( "TEST", comp.selectedLayers[i]);
+    }
+
+     DuAE.endUndoGroup();
 })();
