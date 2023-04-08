9001086995e91daff999d727766bc9594ea434b0
Duduf <git@duduf.com>
2023-04-08 12:14:47 +0200

    added undogroup for bones & pins editing

--- tests.jsx
+++ tests.jsx
@@ -1,9 +1,14 @@
 ﻿(function() {
 
-    #include "inc/api_all.jsxinc"
-    DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
-    DuAEF.enterRunTime();
+    //#include "inc/api_all.jsxinc"
+    //DuAEF.init("Duik tests", "RxLaboratory", "0.0.0");
+    //DuAEF.enterRunTime();
 
     //Duik.Tool.editExpression();
-    Duik.Tool.reloadExpressions();
+    //Duik.Tool.reloadExpressions();
+    var folder = new Folder('X:/');
+    try { folder.execute(); }
+    catch(e) {alert(e);}
+    finally {alert('not ok');}
+    alert('ok');
 })();
