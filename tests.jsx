a3f34cf40a28c22aca0451ecb29212325849f888
Duduf <git@duduf.com>
2023-05-13 14:12:27 +0200

    save ae specific data in OA

--- tests.jsx
+++ tests.jsx
@@ -6,12 +6,14 @@
 
     app.beginUndoGroup("OA Test");
 
-    var p = app.project.activeItem.layer(1).transform.position;
+    var p = app.project.activeItem.layer(1).transform.scale;
 
     var group = OAProperty.fromAEProperty(p);
     //alert(group.toJson());
+
+    //group.toFile('X:/test.oa.json');
     
-    var p2 = app.project.activeItem.layer(2).transform.position; 
+    var p2 = app.project.activeItem.layer(2).transform.scale; 
     group.toAEProperty(p2);
 
     app.endUndoGroup();
