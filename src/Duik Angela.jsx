1d7e7fc780d0b8e22dbef564d41db912d006c500
Duduf <git@duduf.com>
2023-08-10 11:01:51 +0200

    17.2.1  Build

---
+++ src/Duik Angela.jsx
@@ -0,0 +1,10 @@
+(function(thisObj) {
+
+    var mainScriptFile = new File($.fileName);
+    
+    #include "inc/core.jsx"
+    #include "inc/mainPanel.jsx"
+    buildMainPanelUI( ui.mainGroup );
+    #include "inc/ui_show.jsx"
+    
+})(this);
