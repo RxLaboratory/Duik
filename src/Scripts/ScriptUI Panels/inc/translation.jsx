8f146661d692870d5977b1dce39444ea85f84cd8
Duduf <git@duduf.com>
2024-03-26 12:18:34 +0100

    update translations

--- src/Scripts/ScriptUI Panels/inc/translation.jsx
+++ src/Scripts/ScriptUI Panels/inc/translation.jsx
@@ -5,10 +5,12 @@ DuESF.preInitMethods.push(function ()
         // Extract translations
         var outputFolder = DuESF.scriptSettings.file.parent.absoluteURI + '/';
 
-        Duik_de.toFile( outputFolder + 'Duik_de.json', false );
-        Duik_es.toFile( outputFolder + 'Duik_es.json', false );
-        Duik_fr.toFile( outputFolder + 'Duik_fr.json', false );
-        Duik_zh.toFile( outputFolder + 'Duik_zh.json', false );
-        Duik_ru.toFile( outputFolder + 'Duik_ru.json', false );
+        Duik_de_DE.toFile( outputFolder + 'Duik_de.json', false );
+        Duik_eo_UY.toFile( outputFolder + 'Duik_eo.json', false );
+        Duik_es_ES.toFile( outputFolder + 'Duik_es.json', false );
+        Duik_fr_FR.toFile( outputFolder + 'Duik_fr.json', false );
+        Duik_ru_RU.toFile( outputFolder + 'Duik_ru.json', false );
+        Duik_zh_CN.toFile( outputFolder + 'Duik_zh.json', false );
+        Duik_zh_TW.toFile( outputFolder + 'Duik_zh_TW.json', false );
     }
 );
